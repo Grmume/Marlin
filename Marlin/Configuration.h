1b4a635b58190bb07a6e38dc92333455d433c7d4
grmume <charly002@gmx.de>
2021-12-28 15:51:39 +0100

    -- Changed author and machine name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(dippold.com, CR-10 V2)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Grmume, CR-10 V2)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "CR-10 V2"
+#define CUSTOM_MACHINE_NAME "CR-10 V2 BLTouch"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like http://www.uuidgenerator.net/version4
